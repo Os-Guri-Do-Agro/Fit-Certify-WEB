d161b6529dc4b8e54e69258f703b47a8a35a18c2
Luis Gustavo <darkbj13.lg@gmail.com>
2025-07-29 03:59:33 -0300

    feat: implementa parte da page 'Quem Somos'

--- src/pages/QuemSomos.vue
+++ src/pages/QuemSomos.vue
@@ -1,5 +1,124 @@
 <template>
-    <div class="">
-        <h1>olá</h1>
-    </div>
+    <section class="bg-[url('/src/assets/quems-imgs/quems-banner.jpg')] w-full h-[128.4px] md:h-[263px] lg:h-[397px] bg-center bg-cover flex justify-center items-center p-[10px]">
+        <div class="w-full max-w-[1363px] flex items-center justify-center">
+            <div class="w-full max-w-[261px] h-[53px] md:max-w-[406px] md:h-[82px] lg:max-w-[647.5px] lg:h-[131.1px] bg-cyan-400 transform -skew-x-12 flex items-center justify-center text-center text-white font-[600] italic">
+                <h1 class="text-[1.625em] md:text-[2.5em] lg:text-[4.5em]">
+                    Quem somos
+                </h1>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="w-full min-h-[240px] flex justify-center items-center flex-col">
+        <div class="w-full h-[155px] lg:h-[240px] flex justify-center items-center bg-sky-50">
+            <h2 class="text-[1.375em] md:text-[1.75em] lg:text-[2.25em] text-cyan-400 font-[600] italic leading-[34px] p-[10px] text-center">
+                Tecnologia a favor da saúde esportiva
+            </h2>
+        </div>
+
+        <div class="w-full max-w-[1363px] flex items-center justify-center gap-[32px] md:gap-[62px] flex-col-reverse md:flex-row">
+            <div class="flex w-full max-w-[603.2px] p-[10px]">
+                <p class="text-[1em] lg:text-[1.375em] leading-[35px] lg:leading-[50px] font-[400]">
+                    FitCertify365 nasceu para facilitar a vida de quem pratica esportes e precisa manter seus certificados médicos organizados e válidos. Conectamos médicos parceiros, atletas e eventos em uma única plataforma, oferecendo praticidade na gestão de documentos, sem abrir mão da responsabilidade médica e da segurança dos dados.
+                </p>
+            </div>
+            <div class="flex">
+                <img class="w-full max-w-[841px] min-h-[350px] md:min-h-[444px] lg:h-[510px] object-cover" src="../assets/quems-imgs/quems-img-01.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="w-full min-h-[240px] flex justify-center items-center flex-col">
+        <div class="w-full md:min-h-[847px] lg:min-h-[501px] flex justify-center items-center bg-sky-50 flex-col gap-[52px]">
+            <h2 class="text-[1.375em] md:text-[1.75em] lg:text-[2.25em] text-lime-500 font-[600] italic leading-[40px] md:leading-[55px] p-[10px] text-center">
+                Mais do que certificados. Uma plataforma completa de saúde esportiva.
+            </h2>
+            <div class="w-full max-w-[1363px] hidden md:flex items-center justify-center gap-[84px] flex-wrap lg:flex-nowrap">
+                <div class="w-[237px] h-[295px] flex items-center flex-col text-center gap-[52px]">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#88CE0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-check-big-icon lucide-square-check-big"><path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.344"/><path d="m9 11 3 3L22 4"/></svg>
+                    </div>
+                    
+                    <div class="">
+                        <p class="text-[1em] lg:text-[1.25em] leading-[40px]">
+                            Validamos atestados digitais com validade <br> legal
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-[237px] h-[295px] flex items-center flex-col text-center gap-[52px]">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#88CE0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-monitor-icon lucide-monitor"><rect width="20" height="14" x="2" y="3" rx="2"/><line x1="8" x2="16" y1="21" y2="21"/><line x1="12" x2="12" y1="17" y2="21"/></svg>
+                    </div>
+                    
+                    <div class="">
+                        <p class="text-[1em] lg:text-[1.25em] leading-[40px]">
+                            Monitoramos marcadores de saúde ao longo do tempo
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-[237px] h-[295px] flex items-center flex-col text-center gap-[52px]">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#88CE0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-icon lucide-chart-no-axes-column"><line x1="18" x2="18" y1="20" y2="10"/><line x1="12" x2="12" y1="20" y2="4"/><line x1="6" x2="6" y1="20" y2="14"/></svg>
+                    </div>
+                    
+                    <div class="">
+                        <p class="text-[1em] lg:text-[1.25em] leading-[40px]">
+                            Integramos dados de exames e dispositivos inteligentes
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-[237px] h-[295px] flex items-center flex-col text-center gap-[52px]">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#88CE0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-icon lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+                    </div>
+                    
+                    <div class="">
+                        <p class="text-[1em] lg:text-[1.25em] leading-[40px]">
+                            Facilitamos a jornada do atleta e do organizador de eventos
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-[237px] h-[295px] flex items-center flex-col text-center gap-[52px]">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#88CE0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link2-icon lucide-link-2"><path d="M9 17H7A5 5 0 0 1 7 7h2"/><path d="M15 7h2a5 5 0 1 1 0 10h-2"/><line x1="8" x2="16" y1="12" y2="12"/></svg>
+                    </div>
+                    
+                    <div class="">
+                        <p class="text-[1em] lg:text-[1.25em] leading-[40px]">
+                            Criamos conexões com corridas e provas que valorizam a saúde
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            
+
+            <!-- AQUI VAI O CAROUSEL -->
+        </div>
+    </section>
+
+
+    <section class="w-full min-h-[240px] flex justify-between items-center flex-col border-1">
+        <div class="w-full max-w-[1363px] flex items-center justify-center gap-[32px] md:gap-[62px] flex-col-reverse md:flex-row lg:flex-row-reverse bg-lime-100">
+            <div class="flex flex-col w-full max-w-[603.2px] p-[10px] gap-[18px] md:gap-[32px]">
+                <h3 class="text-[1.375em] md:text-[1.75em] lg:text-[2.25em] text-cyan-400 font-[600] italic leading-[40px] md:leading-[55px]">
+                    Humanização, inovação <br> e responsabilidade
+                </h3>
+                <p class="text-[1em] lg:text-[1.375em] leading-[35px] lg:leading-[50px] font-[400]">
+                    Utilizamos criptografia, assinatura digital e integração com dados clínicos reais.Além disso, criamos uma experiência fluida e confiável para quem precisa cuidar da saúde sem burocracia.
+                </p>
+            </div>
+            <div class="flex">
+                <img class="w-full max-w-[841px] min-h-[350px] md:min-h-[444px] lg:h-[510px] object-cover" src="../assets/quems-imgs/quems-img-02.jpg" alt="">
+            </div>
+        </div>
+    </section>
 </template>
