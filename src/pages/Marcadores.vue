8b0f9a0ae251941ccc864070402782edb28d5e30
Luis Gustavo <darkbj13.lg@gmail.com>
2025-07-31 04:07:58 -0300

    chore: adiciona as imagens da page Marcadores

--- src/pages/Marcadores.vue
+++ src/pages/Marcadores.vue
@@ -22,7 +22,7 @@
     </section>
 
 
-    <section class="flex flex-col items-center justify-center bg-cyan-50">
+    <section class="flex flex-col items-center justify-center bg-cyan-50 pb-10">
         <div class="w-full flex flex-col justify-center items-center p-5 md:p-10 lg:h-[223px] bg-white">
             <p class="text-[1em] lg:text-[1.375em] md:text-center leading-[35px]">
                 Tenha acesso aos principais indicadores da sua saúde esportiva em tempo real. <br>
@@ -41,8 +41,11 @@
                 </p>
             </div>
 
-            <div class="flex gap-[22px]">
-                <div class="flex flex-col justify-center w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+
+
+
+            <div class="hidden md:flex w-full max-w-[1340px] gap-[22px] flex-wrap justify-center">
+                <div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
                     <h3 class="text-[1.125em] font-[700] text-lime-500">
                         💓 Frequência Cardíaca de Repouso
                     </h3>
@@ -51,24 +54,144 @@
                     </p>
                 </div>
 
-                <div class="flex flex-col justify-center w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+                <div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
                     <h3 class="text-[1.125em] font-[700] text-lime-500">
-                        💓 Frequência Cardíaca de Repouso
+                        ⚖️ IMC e Composição Corporal
                     </h3>
                     <p class="text-[0.875em text-slate-700">
-                        Identifique padrões de recuperação e esforço com base nos seus batimentos em repouso.
+                        Avaliação de massa, peso e proporção corporal para acompanhamento físico contínuo.
                     </p>
                 </div>
 
-                <div class="flex flex-col justify-center w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+                <div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
                     <h3 class="text-[1.125em] font-[700] text-lime-500">
-                        💓 Frequência Cardíaca de Repouso
+                        🧪 Exames Laboratoriais
                     </h3>
                     <p class="text-[0.875em text-slate-700">
-                        Identifique padrões de recuperação e esforço com base nos seus batimentos em repouso.
+                        Visualize resultados como colesterol, glicemia, ferritina e outros indicadores clínicos.
+                    </p>
+                </div>
+
+
+<div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+                    <h3 class="text-[1.125em] font-[700] text-lime-500">
+                        🫁 VO₂ Máx
+                    </h3>
+                    <p class="text-[0.875em text-slate-700">
+                        Estimativa de consumo máximo de oxigênio — essencial para esportes de resistência.
+                    </p>
+                </div>
+
+                <div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+                    <h3 class="text-[1.125em] font-[700] text-lime-500">
+                        🩺 Pressão Arterial
+                    </h3>
+                    <p class="text-[0.875em text-slate-700">
+                        Acompanhamento regular dos seus níveis de pressão, com registros manuais ou automáticos.
+                    </p>
+                </div>
+
+                <div class="flex flex-col w-full max-w-[320px] h-[178px] md:h-[204px] bg-white rounded-[12px] shadow-xl p-5 gap-[12px]">
+                    <h3 class="text-[1.125em] font-[700] text-lime-500">
+                        📈 Histórico de Certificados
+                    </h3>
+                    <p class="text-[0.875em text-slate-700">
+                        Acompanhe sua linha do tempo de atestados, consultas e avaliações médicas.
                     </p>
                 </div>
             </div>
+
+
+            <!-- COLOCAR UM CAROUSEL AQUI -->
+
+        </div>
+    </section>
+
+
+    <section class="w-full">
+        <div class="">
+                <div class="flex conteiner flex-col items-center md:items-stretch md:flex-row-reverse h-auto md:h-[383px] lg:h-[480px]">
+      <div class="md:w-1/2">
+          <img class="w-full h-[350px] md:h-full object-cover object-bottom" src="../assets/home-imgs/home-img-01.jpg" alt=""> 
+      </div>
+
+      <div class="md:w-1/2 md:h-auto flex flex-col lg:items-center justify-center gap-[24px] md:p-10">
+        <h2 class="text-[1.25em] md:text-[1.75em] lg:text-[2.5em] xl:text-[3em] font-[700] italic text-cyan-400
+        w-full max-w-[461px] lg:max-w-[658px] md:ml-[24px] mt-[27px] leading-[35px] lg:leading-[55px]">
+          Sua saúde validada. <br> Seu esporte garantido.
+        </h2>
+        <p class="text-[0.875em] lg:text-[1em] xl:text-[1.25em]
+        w-full max-w-[250px] md:max-w-[325px] lg:max-w-[658px] xl:max-w-[658px] md:ml-[24px] lg:pr-[24px]">
+          Vai competir? Consulte o seu médico, receba seu certificado e valide ele na FitCertify365. Nós organizar e aplicamos seu certificado nas principais provas do Brasil e do mundo.
+        </p>
+
+        <div class="w-full md:max-w-[461px] lg:max-w-[658px] md:ml-[24px]">
+          <router-link
+            class="text-[0.93em] font-[500] w-[224px] h-[28px] lg:h-[42px] rounded-[30px] border-1 border-cyan-400 text-center text-cyan-400 hover:bg-cyan-400 hover:text-white duration-300 flex items-center justify-center mb-[32px] md:mb-0"
+            to='/quemsomos'
+            >Saiba Mais</router-link
+          >
+        </div>
+
+      </div>
+    </div>
+
+
+
+    <div class="flex conteiner flex-col items-center md:items-stretch md:flex-row h-auto md:h-[383px] lg:h-[480px]">
+      <div class="md:w-1/2">
+          <img class="w-full h-[350px] md:h-full object-cover object-bottom" src="../assets/home-imgs/home-img-02.jpg" alt=""> 
+      </div>
+
+      <div class="md:w-1/2 md:h-auto flex flex-col lg:items-center justify-center gap-[24px] md:p-10 ml-4 md:ml-0">
+        <h2 class="text-[1.25em] md:text-[1.75em] lg:text-[2.5em] xl:text-[3em] font-[700] italic text-lime-500
+        w-full max-w-[461px] lg:max-w-[658px] md:ml-[24px] mt-[27px] leading-[35px] lg:leading-[55px] ">
+          Vai correr? <br>
+          Confira eventos parceiros
+        </h2>
+        <p class="text-[0.875em] lg:text-[1em] xl:text-[1.25em]
+        w-full max-w-[250px] md:max-w-[325px] lg:max-w-[658px] xl:max-w-[658px] md:ml-[24px] lg:pr-[24px]">
+          Após sua consulta com um médico, faça o upload do seu certificado na plataforma e gere o modelo de certificado ideal para sua prova — válido e personalizado nas principais corridas do Brasil e do mundo.
+        </p>
+
+        <div class="w-full md:max-w-[461px] lg:max-w-[658px] md:ml-[24px]">
+          <router-link
+            class="text-[0.93em] font-[500] w-[224px] h-[28px] lg:h-[42px] rounded-[30px] border-1 border-lime-500 text-center text-lime-500 hover:bg-lime-500 hover:text-white duration-300 flex items-center justify-center mb-[32px] md:mb-0"
+            to="/saiba-mais"
+            >Saiba Mais</router-link
+          >
+        </div>
+
+      </div>
+    </div>
+
+
+
+    <div class="flex conteiner flex-col items-center md:items-stretch md:flex-row-reverse h-auto md:h-[383px] lg:h-[480px]">
+      <div class="md:w-1/2">
+          <img class="w-full h-[350px] md:h-full object-cover object-bottom" src="../assets/home-imgs/home-img-03.jpg" alt=""> 
+      </div>
+
+      <div class="md:w-1/2 md:h-auto flex flex-col lg:items-center justify-center gap-[24px] md:p-10 ml-9 md:ml-0">
+        <h2 class="text-[1.25em] md:text-[1.75em] lg:text-[2.5em] xl:text-[3em] font-[700] italic text-cyan-400
+        w-full max-w-[461px] lg:max-w-[658px] md:ml-[24px] mt-[27px] leading-[35px] lg:leading-[55px]">
+          Acompanhe seus <br> indicadores em tempo real.
+        </h2>
+        <p class="text-[0.875em] lg:text-[1em] xl:text-[1.25em]
+        w-full max-w-[250px] md:max-w-[325px] lg:max-w-[658px] xl:max-w-[658px] md:ml-[24px] lg:pr-[24px]">
+          Acesse indicadores, histórico de exames e alertas inteligentes sobre sua saúde esportiva. Tudo integrado em um só painel.
+        </p>
+
+        <div class="w-full md:max-w-[461px] lg:max-w-[658px] md:ml-[24px]">
+          <router-link
+            class="text-[0.93em] font-[500] w-[224px] h-[28px] lg:h-[42px] rounded-[30px] border-1 border-cyan-400 text-center text-cyan-400 hover:bg-cyan-400 hover:text-white duration-300 flex items-center justify-center mb-[32px] md:mb-0"
+            to="/saiba-mais"
+            >Saiba Mais</router-link
+          >
+        </div>
+
+      </div>
+    </div>
         </div>
     </section>
 </template>
