d107c9a2862658573fc678920a227ef60d270caa
Luis Gustavo <darkbj13.lg@gmail.com>
2025-08-20 05:56:46 -0300

    feat: resolve bug de recarregamento das subpages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 // src/router/index.ts
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import { nextTick } from 'vue'
 import Home from '../pages/Home.vue'
@@ -36,7 +36,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   
   scrollBehavior() {
