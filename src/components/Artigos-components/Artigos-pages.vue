87d4afe56a68a8a0b80c615a5ee1ddafa0e7a6cc
Luis Gustavo <darkbj13.lg@gmail.com>
2025-08-20 18:33:27 -0300

    feat: coloca lading skeleton em artigos

--- src/components/Artigos-components/Artigos-pages.vue
+++ src/components/Artigos-components/Artigos-pages.vue
@@ -2,79 +2,87 @@
   <div>
     <!-- Grid de Cards -->
     <div class="grid md:grid-cols-2 md:grid-rows-1 place-items-center w-full gap-10 md:gap-3 lg:gap-7 justify-center">
-      <div class="w-full md:max-w-[558px] shadow-xl md:shadow-none pb-5 md:pb-0 rounded-lg lg:max-w-full min-h-[680px] md:min-h-[700px] lg:min-h-[850px] xl:min-h-[750px]"
-           v-for="item in Artigos.data" :key="item.id">
-        <div class="flex">
-          <img class="w-full max-w-[558px] lg:max-w-full h-[433px] object-cover"
-               :src="item.imagensArtigo.find((i: any) => i.isBanner == false)?.imagemUrl" alt="">
-        </div>
-
-        <div class="flex flex-col justify-between gap-3 h-full mt-5 lg:mt-7 min-h-[230px] md:min-h-[280px] lg:min-h-[320px]">
-          <div>
-            <h1 class="text-[1.3em] lg:text-[2.125em] font-[600] italic text-cyan-400 max-w-[525px] lg:leading-[52px] md:leading-[30px] leading-[26px]">
-              {{ item.titulo }}
-            </h1>
+      
+      <!-- Skeletons quando está carregando -->
+      <template v-if="isLoading">
+        <div 
+          v-for="n in itemsPerPage" 
+          :key="'skeleton-' + n"
+          class="w-full md:max-w-[558px] shadow-xl md:shadow-none pb-5 md:pb-0 rounded-lg lg:max-w-full min-h-[680px] md:min-h-[700px] lg:min-h-[850px] xl:min-h-[750px] animate-pulse "
+        >
+          <div class="flex">
+            <div class="w-full max-w-[558px] lg:max-w-full h-[433px] bg-gray-300 rounded"></div>
           </div>
 
-          <div>
-            <p class="text-[0.85em] lg:text-[1.25em] w-full max-w-[551px] leading-[26px] lg:leading-[36px]">
-              {{ item.subTitulo }}
-            </p>
-          </div>
-
-          <div>
-            <RouterLink :to="{ name: 'ArtigoDetalhe', params: { id: item.id } }"
-              class="w-full max-w-[150px] lg:max-w-[212.6px] h-[36.3px] rounded-[30px] flex items-center justify-center text-[0.9em] text-cyan-400 font-[500] border border-cyan-400 duration-300 hover:bg-cyan-400 hover:text-white cursor-pointer"
-              @click="emit('refresh-page', item.id)">
-              LEIA MAIS
-            </RouterLink>
+          <div class="flex flex-col justify-between gap-3 h-full mt-5 lg:mt-7 min-h-[230px] md:min-h-[280px] lg:min-h-[320px] px-3 md:px-0">
+            <div class="h-7 lg:h-10 bg-gray-300 rounded md:w-3/4"></div>
+            <div class="h-5 lg:h-7 bg-gray-300 rounded w-full mt-3"></div>
+            <div class="h-5 lg:h-7 bg-gray-300 rounded md:w-5/6 mt-2"></div>
+            <div class="h-[36px] bg-gray-300 rounded-full w-[558px] lg:w-[212px] mt-4"></div>
           </div>
         </div>
-      </div>
+      </template>
+
+      <!-- Cards de Artigos -->
+      <template v-else>
+        <div 
+          class="w-full md:max-w-[558px] shadow-xl md:shadow-none pb-5 md:pb-0 rounded-lg lg:max-w-full min-h-[680px] md:min-h-[700px] lg:min-h-[850px] xl:min-h-[750px]"
+          v-for="item in Artigos.data" 
+          :key="item.id"
+        >
+          <div class="flex">
+            <img 
+              class="w-full max-w-[558px] lg:max-w-full h-[433px] object-cover"
+              :src="item.imagensArtigo.find((i: any) => i.isBanner == false)?.imagemUrl" 
+              alt=""
+            >
+          </div>
+
+          <div class="flex flex-col justify-between gap-3 h-full mt-5 lg:mt-7 min-h-[230px] md:min-h-[280px] lg:min-h-[320px] px-3 md:px-0">
+            <div>
+              <h1 class="text-[1.3em] lg:text-[2.125em] font-[600] italic text-cyan-400 max-w-[525px] lg:leading-[52px] md:leading-[30px] leading-[26px]">
+                {{ item.titulo }}
+              </h1>
+            </div>
+
+            <div>
+              <p class="text-[0.85em] lg:text-[1.25em] w-full max-w-[551px] leading-[26px] lg:leading-[36px]">
+                {{ item.subTitulo }}
+              </p>
+            </div>
+
+            <div>
+              <RouterLink 
+                :to="{ name: 'ArtigoDetalhe', params: { id: item.id } }"
+                class="w-full max-w-[150px] lg:max-w-[212.6px] h-[36.3px] rounded-[30px] flex items-center justify-center text-[0.9em] text-cyan-400 font-[500] border border-cyan-400 duration-300 hover:bg-cyan-400 hover:text-white cursor-pointer"
+                @click="emit('refresh-page', item.id)"
+              >
+                LEIA MAIS
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+      </template>
     </div>
 
     <!-- Paginação -->
-
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-10">
       <el-pagination
-      v-model:current-page="currentPage"
-      v-model:page-size="itemsPerPage"
-      :total="totalItens"
-      :pager-count="totalPages"
-      layout="prev, pager, next"
-      background
-      @current-change="buscarArtigos"
-    />
+        v-model:current-page="currentPage"
+        v-model:page-size="itemsPerPage"
+        :total="totalItens"
+        :pager-count="totalPages"
+        layout="prev, pager, next"
+        background
+        @current-change="buscarArtigos"
+      />
     </div>
-
-    </div>
-
-    <!-- <div class="flex items-center md:justify-center gap-4 mt-6 flex-col md:flex-row mt-10 md:mt-20">
-      <div class="flex md:hidden">
-        <span class="flex items-center text-cyan-400"> {{ currentPage }} - {{ totalPages }}</span>
-      </div>
-
-      <div class="flex  gap-4">
-              <button @click="goToFirstPage" :disabled="currentPage === 1"
-              className="border-1 border-cyan-400 text-cyan-400 cursor-pointer hover:bg-cyan-400 hover:text-white duration-300 w-15 md:w-17 h-10 md:h-12 flex items-center justify-center rounded-[6px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-left-icon lucide-chevrons-left"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg></button>
-      <button @click="prevPage" :disabled="currentPage === 1"
-              className="border-1 border-cyan-400 text-cyan-400 cursor-pointer hover:bg-cyan-400 hover:text-white duration-300 w-15 md:w-17 h-10 md:h-12 flex items-center justify-center rounded-[6px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg></button>
-      <span class="flex items-center hidden md:flex text-cyan-400"> {{ currentPage }} - {{ totalPages }}</span>
-      <button  @click="nextPage" :disabled="currentPage === totalPages"
-              className="border-1 border-cyan-400 text-cyan-400 cursor-pointer hover:bg-cyan-400 hover:text-white duration-300 w-15 md:w-17 h-10 md:h-12 flex items-center justify-center rounded-[6px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></button>
-      <button @click="goToLastPage" :disabled="currentPage === totalPages"
-              className="border-1 border-cyan-400 text-cyan-400 cursor-pointer hover:bg-cyan-400 hover:text-white duration-300 w-15 md:w-17 h-10 md:h-12 flex items-center justify-center rounded-[6px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-right-icon lucide-chevrons-right"><path d="m6 17 5-5-5-5"/><path d="m13 17 5-5-5-5"/></svg></button>
-      </div>
-
-
-    </div> -->
-  <!-- </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue'
 import ArtigoService from '../../services/Artigos/artigos-service'
-
 
 const emit = defineEmits<{
   'refresh-page': [id: string]
@@ -82,6 +90,7 @@ const emit = defineEmits<{
 
 // Dados
 const Artigos = ref<{ data: any[] }>({ data: [] })
+const isLoading = ref(false)
 
 // Paginação
 const currentPage = ref(1)
@@ -91,53 +100,28 @@ const totalPages = ref(1)
 const totalItens = ref(0)
 
 async function buscarArtigos() {
-  const response = await ArtigoService.getAllPaginated(
-    currentPage.value,        
-    itemsPerPage,         
-    categoriaArtigoId.value,  
-    false
-  )
-  const data = response.data
-  totalItens.value=data.total
-  Artigos.value.data=data.itens
-  totalPages.value=data.totalPages
+  try {
+    isLoading.value = true
+    const response = await ArtigoService.getAllPaginated(
+      currentPage.value,        
+      itemsPerPage,         
+      categoriaArtigoId.value,  
+      false
+    )
+    const data = response.data
+    totalItens.value = data.total
+    Artigos.value.data = data.itens
+    totalPages.value = data.totalPages
+  } finally {
+    isLoading.value = false
+  }
 }
 
-
-onMounted(async () => {
+onMounted(() => {
   buscarArtigos()
 })
 
 watch([currentPage, categoriaArtigoId], () => {
-  buscarArtigos();
-});
-
-// const totalPages = computed(() => Math.ceil(Artigos.value.data.length / itemsPerPage))
-// const paginatedItems = computed(() => {
-//   const start = (currentPage.value - 1) * itemsPerPage
-//   const end = start + itemsPerPage
-//   return Artigos.value.data.slice(start, end)
-// })
-
-// Funções de navegação
-// const nextPage = () => {
-//   if (currentPage.value < totalPages.value) currentPage.value++
-// }
-
-// const prevPage = () => {
-//   if (currentPage.value > 1) currentPage.value--
-// }
-
-// const goToFirstPage = () => {
-//   currentPage.value = 1
-// }
-
-// const goToLastPage = () => {
-//   currentPage.value = totalPages.value
-// }
-
-// Buscar artigos e filtrar apenas isDesktop = true
-
-
+  buscarArtigos()
+})
 </script>
-
