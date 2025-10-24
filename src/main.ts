1feed9c4363e0c7e43c8e16ee258f4b418b242d6
Luis Gustavo <darkbj13.lg@gmail.com>
2025-10-23 21:58:04 -0300

    feat: adiciona nova página de depoimentos

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import './style.css'
 import router from './router/index'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
+import { register } from 'swiper/element/bundle'
 
 
+
+register();
 
 const app = createApp(App);
 app.use(router)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,55 +1,64 @@
 // src/router/index.ts
-import { createRouter, createWebHashHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
-import { nextTick } from 'vue'
-import Home from '../pages/Home.vue'
-import QuemSomos from '../pages/QuemSomos.vue'
-import Certificados from '../pages/Certificados.vue'
-import Marcadores from '../pages/Marcadores.vue'
-import Eventos from '../pages/Eventos.vue'
-import Artigos from '../pages/Artigos.vue'
-import Marketplace from '../pages/Marketplace.vue'
-import Contato from '../pages/Contato.vue'
-import ArtigoDetalhes from '../pages/ArtigoDetalhes.vue'  
-import EventoDetalhes from '../pages/EventosDetalhes.vue'
-import MarketplaceDetalhes from '../pages/MarketplaceDetalhes.vue' 
-import Login from '../pages/Login.vue' 
-import Cadastro from '../pages/Cadastro.vue'
-import Teste from '../pages/teste.vue'
-import TermosPrivacidade from '../pages/TermosPrivacidade.vue'
-
+import { createRouter, createWebHashHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import { nextTick } from "vue";
+import Home from "../pages/Home.vue";
+import QuemSomos from "../pages/QuemSomos.vue";
+import Certificados from "../pages/Certificados.vue";
+import Marcadores from "../pages/Marcadores.vue";
+import Eventos from "../pages/Eventos.vue";
+import Artigos from "../pages/Artigos.vue";
+import Marketplace from "../pages/Marketplace.vue";
+import Contato from "../pages/Contato.vue";
+import ArtigoDetalhes from "../pages/ArtigoDetalhes.vue";
+import EventoDetalhes from "../pages/EventosDetalhes.vue";
+import MarketplaceDetalhes from "../pages/MarketplaceDetalhes.vue";
+import Login from "../pages/Login.vue";
+import Cadastro from "../pages/Cadastro.vue";
+import Teste from "../pages/teste.vue";
+import TermosPrivacidade from "../pages/TermosPrivacidade.vue";
+import Depoimentos from "../pages/depoimentos.vue";
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', component: Home },
-  { path: '/quemsomos', name: 'QuemSomos', component: QuemSomos },
-  { path: '/certificados', name: 'Certificados', component: Certificados },
-  { path: '/marcadores', name: 'Marcadores', component: Marcadores },
-  { path: '/eventos', name: 'Eventos', component: Eventos },
-  { path: '/artigos', name: 'Artigos', component: Artigos },
-  { path: '/marketplace', name: 'Marketplace', component: Marketplace},
-  { path: '/contato', name: 'Contato', component: Contato},
-  {path: '/login', name: 'Login', component: Login},
-  {path: '/cadastro', name: 'Cadastro', component: Cadastro},
-  {path: '/artigo/:id', name: 'ArtigoDetalhe', component: ArtigoDetalhes,},
-  {path: '/evento/:id', name: 'EventoDetalhe', component: EventoDetalhes,},
-  {path: '/marketplace/:id', name: 'MarketDetalhes', component: MarketplaceDetalhes,},
-  {path: '/teste', name: 'Teste', component: Teste,},
-  {path: '/termosprivacidade', name: 'TermosPrivacidade', component: TermosPrivacidade,}
-]
+  { path: "/", component: Home },
+  { path: "/quemsomos", name: "QuemSomos", component: QuemSomos },
+  { path: "/certificados", name: "Certificados", component: Certificados },
+  { path: "/marcadores", name: "Marcadores", component: Marcadores },
+  { path: "/eventos", name: "Eventos", component: Eventos },
+  { path: "/artigos", name: "Artigos", component: Artigos },
+  { path: "/marketplace", name: "Marketplace", component: Marketplace },
+  { path: "/contato", name: "Contato", component: Contato },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/cadastro", name: "Cadastro", component: Cadastro },
+  { path: "/artigo/:id", name: "ArtigoDetalhe", component: ArtigoDetalhes },
+  { path: "/evento/:id", name: "EventoDetalhe", component: EventoDetalhes },
+  {
+    path: "/marketplace/:id",
+    name: "MarketDetalhes",
+    component: MarketplaceDetalhes,
+  },
+  { path: "/teste", name: "Teste", component: Teste },
+  {
+    path: "/termosprivacidade",
+    name: "TermosPrivacidade",
+    component: TermosPrivacidade,
+  },
+  { path: "/depoimentos", name: "Depoimentos", component: Depoimentos },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-  
+
   scrollBehavior() {
     nextTick(() => {
       window.scrollTo({
         top: 0,
-        behavior: 'smooth'
-      })
-    })
-    return false 
-  }
-})
+        behavior: "smooth",
+      });
+    });
+    return false;
+  },
+});
 
-export default router
+export default router;
