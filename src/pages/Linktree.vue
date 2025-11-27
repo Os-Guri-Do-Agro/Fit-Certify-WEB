d35c506632e8dad37ad876e19acb3760a6c0b1ed
Luis Gustavo <darkbj13.lg@gmail.com>
2025-11-27 18:46:32 -0300

    style: update Linktree layout with rounded corners and hover effects for social media links and buttons

--- src/pages/Linktree.vue
+++ src/pages/Linktree.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full min-h-screen  flex flex-col items-center py-10 px-1 md:px-4">
         <img src="/Logo-Grande.png" alt="FitCertify365" class="h-12 mb-8" />
-    <div class="bg-gray-100 flex flex-col items-center justify-center py-10 px-2 md:px-10 rounded-xl">
-    <div class="bg-white rounded-2xl p-2 md:p-8 max-w-md w-full text-center relative pt-16 pb-8 my-15 border-2 border-gray-200">
+    <div class="bg-gray-100 flex flex-col items-center justify-center py-10 px-2 md:px-10 ">
+    <div class="bg-white rounded-4xl p-2 md:p-8 max-w-md w-full text-center relative pt-16 pb-8 my-15 border-2 border-gray-200">
     <div class="absolute -top-12 left-1/2 transform -translate-x-1/2">
         <div class="w-24 h-24 rounded-full overflow-hidden">
                <img src="/logo-fundoAzul.png" alt="FitCertify Logo" class="w-full h-full object-cover" /> 
@@ -17,35 +17,35 @@
     </p>
     
     
-    <a href="https://fitcertify365.com/#/" target="_blank" class="absolute -bottom-6 left-1/2 transform -translate-x-1/2 bg-sky-500 text-white px-6 py-3 rounded-full font-semibold shadow hover:bg-sky-600 transition cursor-pointer text-[0.85em] md:text-[1em] w-full max-w-[60%]">
+    <a href="https://fitcertify365.com/#/" target="_blank" class="absolute -bottom-6 left-1/2 transform -translate-x-1/2 bg-sky-500 text-white px-6 py-3 rounded-full font-semibold shadow hover:bg-sky-600 transition cursor-pointer text-[0.85em] md:text-[1em] w-full max-w-[60%] duarion-300">
     ACESSE NOSSO SITE
     </a>
     
     </div>
 
     <div class="flex flex-wrap justify-center gap-3 mt-6 text-3xl">
-        <a href="https://www.youtube.com/@FitCertify365" target="_blank" class="bg-red-500 w-15 h-15 rounded-4xl flex items-center justify-center">
+        <a href="https://www.youtube.com/@FitCertify365" target="_blank" class="bg-red-500 w-15 h-15 rounded-4xl flex items-center justify-center hover:-translate-y-3 duration-300">
                 <i class="text-white fab fa-youtube"></i>
         </a>
 
-        <a href="https://www.facebook.com/profile.php?id=61581850576820" target="_blank" class="bg-blue-500 w-15 h-15 rounded-4xl flex items-center justify-center">
+        <a href="https://www.facebook.com/profile.php?id=61581850576820" target="_blank" class="bg-blue-500 w-15 h-15 rounded-4xl flex items-center justify-center hover:-translate-y-3 duration-300">
             <i class="text-white fab fa-facebook"></i>
         </a>
-        <a href="https://wa.me/5521400495694?text=Olá! Gostaria de saber mais sobre a FitCertify365." target="_blank" class="bg-green-500 w-15 h-15 rounded-4xl flex items-center justify-center">
+        <a href="https://wa.me/5521400495694?text=Olá! Gostaria de saber mais sobre a FitCertify365." target="_blank" class="bg-green-500 w-15 h-15 rounded-4xl flex items-center justify-center hover:-translate-y-3 duration-300">
             <i class="text-white fab fa-whatsapp"></i>
         </a>
-        <a href="https://fitcertify365.com/#/contato" target="_blank" class="bg-sky-500 w-15 h-15 rounded-4xl flex items-center justify-center">
+        <a href="https://fitcertify365.com/#/contato" target="_blank" class="bg-sky-500 w-15 h-15 rounded-4xl flex items-center justify-center hover:-translate-y-3 duration-300">
             <i class="text-white fas fa-envelope"></i>
         </a>
     
-        <a href="https://fitcertify365.com/#/" target="_blank" class="bg-lime-500 w-15 h-15 rounded-4xl flex items-center justify-center">
+        <a href="https://fitcertify365.com/#/" target="_blank" class="bg-lime-500 w-15 h-15 rounded-4xl flex items-center justify-center hover:-translate-y-3 duration-300">
             <i class="text-white fas fa-globe"></i>
         </a>
     
     </div>
     
     <div class="w-full flex flex-col gap-4 my-10">
-           <a href="https://play.google.com/store/apps/details?id=com.nicolascastela.fitcertify" target="_blank" class="bg-white rounded-full shadow flex items-center justify-center w-full h-20 gap-2">
+           <a href="https://play.google.com/store/apps/details?id=com.nicolascastela.fitcertify" target="_blank" class="bg-white rounded-full shadow flex items-center justify-center w-full h-20 gap-2 duration-200 hover:bg-gray-300">
         <div class="">
             <img src="/googlePlay.png" alt="logo">
         </div>
@@ -55,7 +55,7 @@
         </div>
     </a> 
 
-    <a href="https://apps.apple.com/br/app/fitcertify365/id6752969790" target="_blank" class="bg-white rounded-full shadow flex items-center justify-center w-full h-20 gap-2">
+    <a href="https://apps.apple.com/br/app/fitcertify365/id6752969790" target="_blank" class="bg-white rounded-full shadow flex items-center justify-center w-full h-20 gap-2 duration-200 hover:bg-gray-300">
         <div class="">
             <img src="/appleStore.png" alt="logo">
         </div>
